b344dd177343951f23c501f2ab0b32ee2ec1f5e2
fuser312 <sherltonGT40000>
2020-02-20 09:32:28 +0530

    Done with m*n complexity

--- bin/main.dart
+++ bin/main.dart
@@ -7,10 +7,47 @@
 //  In this example, assume nodes with the same value are the exact same
 //  node objects.
 
+import 'dart:collection';
 
 class Node {
   int val;
   Node next;
 
   Node(this.val);
+}
+
+int intersectingNode(Node a, Node b) {
+  var tempA = a;
+  var tempB = b;
+  while (tempA != null) {
+    while (tempB != null) {
+      if (tempB.val == tempA.val) {
+        return tempA.val;
+      } else {
+        tempB = tempB.next;
+      }
+      tempA = tempA.next;
+    }
+  }
+  return null;
+}
+
+LinkedList thisList;
+
+void main() {
+  Node first = Node(3);
+  Node second = Node(7);
+  Node third = Node(8);
+  Node fourth = Node(10);
+
+  Node fifth = Node(99);
+  Node sixth = Node(1);
+
+  first.next = second;
+  second.next = third;
+  third.next = fourth;
+  fifth.next = sixth;
+  sixth.next = third;
+
+  print(intersectingNode(first, fifth));
 }
